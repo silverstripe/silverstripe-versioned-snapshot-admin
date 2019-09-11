1f5716478d608c69a587b53aa61efdde5b57335a
Adrian Humphreys <adrian.humphreys@silverstripe.com>
2019-09-12 06:51:14 +1200

    Work with fluent

--- src/ReadSnapshotHistory.php
+++ src/ReadSnapshotHistory.php
@@ -6,7 +6,10 @@ use Exception;
 use GraphQL\Type\Definition\ResolveInfo;
 use SilverStripe\GraphQL\OperationResolver;
 use SilverStripe\GraphQL\Scaffolding\Scaffolders\ListQueryScaffolder;
+use SilverStripe\ORM\ArrayList;
+use SilverStripe\ORM\DataList;
 use SilverStripe\ORM\DataObject;
+use SilverStripe\Snapshots\SnapshotHasher;
 use SilverStripe\Snapshots\SnapshotPublishable;
 use SilverStripe\Versioned\Versioned;
 
@@ -16,6 +19,8 @@ if (!class_exists(ListQueryScaffolder::class)) {
 
 class ReadSnapshotHistory extends ListQueryScaffolder implements OperationResolver
 {
+    use SnapshotHasher;
+
     /**
      * ReadOperationScaffolder constructor.
      *
@@ -52,7 +57,17 @@ class ReadSnapshotHistory extends ListQueryScaffolder implements OperationResolv
         $list = $list->sort('"LastEdited"', 'DESC');
 
         $this->extend('updateList', $list, $object, $args, $context, $info);
-
-        return $list;
+        // To check if the items are the full versions we compare their hashes against the objects hash
+        // this is used in the frontend to show the user if the snapshot is from the object itself
+        // or one of it's children
+        // The reason for doing this here is so that it behaves nicely with fluent, ideally this would 
+        // move to the frontend
+        $objectHash = static::hashObjectForSnapshot($object);
+        $listWithAlterations = ArrayList::create();
+        foreach ($list as $item) {
+            $item->IsFullVersion = $item->OriginHash === $objectHash;
+            $listWithAlterations->push($item);
+        }
+        return $listWithAlterations;
     }
 }
