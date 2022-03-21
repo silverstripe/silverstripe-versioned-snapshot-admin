ec0cd3b965ed3aaf368c7faf2adbbc1575061932
Mojmir Fendek <mfendek@silverstripe.com>
2022-03-21 14:30:08 +1300

    NEW: Replace raw queries with ORM calls.

--- src/SnapshotPublishableExtension.php
+++ src/SnapshotPublishableExtension.php
@@ -2,39 +2,23 @@
 
 namespace SilverStripe\SnapshotAdmin;
 
+use Exception;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\ORM\DataList;
-use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\DataQuery;
-use SilverStripe\ORM\DB;
-use SilverStripe\Snapshots\Snapshot;
-use SilverStripe\Snapshots\SnapshotHasher;
-use SilverStripe\Snapshots\SnapshotItem;
 
 class SnapshotPublishableExtension extends DataExtension
 {
-    use SnapshotHasher;
-
     /**
      * We want to get the version that the snapshots should point to
+     * @throws Exception
      */
     public function updateRelevantSnapshots(DataList &$result)
     {
-        $snapshotTable = DataObject::getSchema()->tableName(Snapshot::class);
-        $itemTable = DataObject::getSchema()->tableName(SnapshotItem::class);
-        $hash = static::hashObjectForSnapshot($this->owner);
-        $result = $result->alterDataQuery(function (DataQuery $query) use ($hash, $snapshotTable, $itemTable) {
-            $subquery = <<<SQL
-                (
-                  SELECT "Version" FROM "$itemTable"
-                  WHERE "ObjectHash" = ? AND "$itemTable"."SnapshotID" = "$snapshotTable"."ID"
-                )
-SQL;
-            $query->selectField(
-                DB::inline_parameters($subquery, [$hash]),
-                'baseVersion'
-            );
-            return $query;
-        });
+        $result = $result
+            ->applyRelation('Items.Version', $itemVersionColumn)
+            ->alterDataQuery(static function (DataQuery $query) use ($itemVersionColumn) {
+                $query->selectField($itemVersionColumn, 'baseVersion');
+            });
     }
 }
